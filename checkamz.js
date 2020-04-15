8b50a2821dbb84cab4dfa5716b4bef485f7b37a9
Jade Lau <jadeyslau@gmail.com>
2020-04-15 12:06:28 +0100

    Corrected date placement

--- checkamz.js
+++ checkamz.js
@@ -1,14 +1,11 @@
-const puppeteer  = require('puppeteer');
-const $ = require('cheerio');
-const telegram = require('telegram-bot-api');
-const CronJob = require('cron').CronJob;
+const puppeteer = require('puppeteer');
+const $         = require('cheerio');
+const telegram  = require('telegram-bot-api');
+const CronJob   = require('cron').CronJob;
 
-const tgram = require('./details').telegram;
-
-var d = new Date();
-var now = d.toLocaleDateString('en-UK')+' '+d.toLocaleTimeString('en-UK');
-
-const url = 'https://www.amazon.co.uk/gp/offer-listing/B081W4XHMZ/ref=olp_twister_all?ie=UTF8&mv_edition=all&mv_platform_for_display=0';
+const tgram  = require('./details').telegram;
+const url    = 'https://www.amazon.co.uk/gp/offer-listing/B081W4XHMZ/ref=olp_twister_all?ie=UTF8&mv_edition=all&mv_platform_for_display=0';
+const domain = 'Amazon.co.uk';
 
 (async () => {
     console.log('init');
@@ -18,7 +15,11 @@ const url = 'https://www.amazon.co.uk/gp/offer-listing/B081W4XHMZ/ref=olp_twiste
 async function startTracking() {
     const page = await configureBrowser();
 
-    let job = new CronJob('*/5 * * * * *', function() { //runs every 5 secs in this config
+    let job = new CronJob('*/10 * * * * *', function() { //runs every 5 secs in this config
+      var d = new Date();
+      var now = d.toLocaleDateString('en-UK')+' '+d.toLocaleTimeString('en-UK');
+
+      console.log('Starting at: '+now);
       checkPrice(page);
     }, null, true, null, null, true);
     job.start();
@@ -32,10 +33,10 @@ async function configureBrowser() {
 }
 
 async function checkPrice(page) {
-
+    await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
     let html = await page.evaluate(() => document.body.innerHTML);
     // console.log(html);
-    console.log(now+' Checking price');
+    console.log('Checking price on '+domain);
 
     $('.olpOfferPrice', html).each(function() {
         let price = $(this).text();
